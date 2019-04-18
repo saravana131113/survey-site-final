6617a224361ab6bb2e0b8100318fe8b42f75fd7c
saravanan <saravana9217@gmail.com>
2019-04-17 20:32:43 -0400

    first commit

---
+++ test/answer_test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var Answer = require('../models/answer');
+
+describe('answer', function(done) {
+  it('should return answer choices of a question', function() {
+    var answer = new Answer({question_id: '5956f01fb04c598b29b118a2'});
+    answer.getAllAnswers(function(res) {
+      console.log('res', res)
+      expect(res).to.be.true;
+      done();
+    });
+  });
+});
